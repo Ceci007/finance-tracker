0944a9d0593220ed923b37bf1272badba83d81bb
solbencas <cecibenitezca@gmail.com>
2020-11-05 13:52:53 -0300

    Add stock model and iex api key

---
+++ app/models/stock.rb
@@ -0,0 +1,11 @@
+class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    ) 
+
+    client.price(ticker_symbol)
+  end
+end
