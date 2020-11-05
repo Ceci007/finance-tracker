f084b712b628defc8714f7b7672fb3b98e7e031a
solbencas <cecibenitezca@gmail.com>
2020-11-05 17:24:59 -0300

    Implementing search stock functionality

--- Gemfile
+++ Gemfile
@@ -25,7 +25,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 gem 'devise-bootstrap-views', '~> 1.0'
-gem 'iex-ruby-client'
+gem 'iex-ruby-client' 
+gem "font-awesome-rails"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,10 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     ) 
 
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception 
+      return nil
+    end
   end
 end
