e9c21810650573498b56767323172b5142cd055b
solbencas <cecibenitezca@gmail.com>
2020-11-06 17:18:32 -0300

    Add show user/friend profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  def show 
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end 
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
-  resources :friendships, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy] 
+  resources :users, only: [:show]
 end
