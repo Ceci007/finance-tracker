bd14c4b922779a6f720bf5e5cedf638f59efc28a
solbencas <cecibenitezca@gmail.com>
2020-11-06 16:34:31 -0300

    Implement remove tracking functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,35 @@ class User < ApplicationRecord
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
   end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back 
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users) 
+    users.reject { |user| user.id == self.id }
+  end
+
+  def not_friends_with?(friend_id) 
+    !self.friends.where(id: friend_id).exists?
+  end
 end
